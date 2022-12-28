5911e4f0c577fec7051adb83e92a1de0f0e1791e
etienne1698 <etienne.robert1698@gmail.com>
2022-12-28 18:36:05 +0100

    up

--- src/routes.ts
+++ src/routes.ts
@@ -6,9 +6,7 @@ export default function Routes(app: Application) {
     fs.readdir(path.join(__dirname, "api"), (err, foldersNames) => {
         if (!err) {
             foldersNames.forEach(folder => {
-                if (fs.existsSync(path.join(__dirname, `./api/${folder}/index.ts`))) {
-                    app.use(`/${folder}`, require(`./api/${folder}`)?.default);
-                }
+                app.use(`/${folder}`, require(`./api/${folder}`)?.default);
             })
         }
     })
