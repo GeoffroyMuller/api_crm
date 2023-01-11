4db215cfb1c69c86be3c98e4d082a3ae19565f70
geoff <geoffroym2@gmail.com>
2023-01-11 21:56:13 +0100

    fix json

--- src/api/products_real/product_real.service.ts
+++ src/api/products_real/product_real.service.ts
@@ -1,7 +1,28 @@
-import serviceFactory from "../../core/service"
+import serviceFactory from "../../core/service";
 import ProductReal from "./product_real.model";
 
-
 const productRealService = serviceFactory(ProductReal);
+
+productRealService.create = async (body: any) => {
+  return (await ProductReal.query().upsertGraphAndFetch(
+    {
+      ...body,
+      product_real_fields: body.product_real_fields.map((elem: any)=>{
+        return {...elem, value: JSON.stringify(elem.value)}
+      })
+    },
+    { relate: true }
+  )) as unknown as ProductReal;
+};
+
+productRealService.update = async (body: any) => {
+  return (await ProductReal.query().upsertGraphAndFetch(
+    {
+      id: body.id,
+      ...body,
+    },
+    { relate: true, unrelate: true }
+  )) as unknown as ProductReal;
+};
 
 export default productRealService;
