cae24a195552e4792b2276e2b3a330330b31b21e
etienne1698 <etienne.robert1698@gmail.com>
2023-01-11 23:04:11 +0100

    up

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -3,7 +3,7 @@ import { Model, QueryBuilderType } from "objection";
 export function defaultHandleFilters<T extends Model>(query: QueryBuilderType<T>, filters: any): QueryBuilderType<T> {
     if (filters?.$eq != null && typeof filters.$eq === 'object') {
         for (const key of Object.keys(filters.$eq)) {
-            query.where(key, filters.$eq[key]);
+            query.where(`${query.modelClass().tableName}.${key}`, filters.$eq[key]);
         }
     }
     return query;
