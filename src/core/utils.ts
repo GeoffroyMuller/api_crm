bc7f45f706649c1678289f868d4f8daab655cda5
etienne1698 <etienne.robert1698@gmail.com>
2023-01-11 23:17:24 +0100

    filter contains

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,10 +1,34 @@
 import { Model, QueryBuilderType } from "objection";
 
-export function defaultHandleFilters<T extends Model>(query: QueryBuilderType<T>, filters: any): QueryBuilderType<T> {
-    if (filters?.$eq != null && typeof filters.$eq === 'object') {
-        for (const key of Object.keys(filters.$eq)) {
-            query.where(`${query.modelClass().tableName}.${key}`, filters.$eq[key]);
-        }
+export function defaultHandleFilters<T extends Model>(
+  query: QueryBuilderType<T>,
+  filters: any
+): QueryBuilderType<T> {
+  if (filters?.$eq != null && typeof filters.$eq === "object") {
+    for (const key of Object.keys(filters.$eq)) {
+      const value = filters.$eq[key];
+      if (
+        value == null ||
+        typeof value === "string" ||
+        typeof value === "number" ||
+        typeof value === "boolean"
+      ) {
+        query.where(`${query.modelClass().tableName}.${key}`, value);
+      }
     }
-    return query;
+  }
+  if (filters?.$contains != null && typeof filters.$contains === "object") {
+    for (const key of Object.keys(filters.$contains)) {
+      const value = filters.$contains[key];
+      if (
+        value == null ||
+        typeof value === "string" ||
+        typeof value === "number" ||
+        typeof value === "boolean"
+      ) {
+        query.where(`${query.modelClass().tableName}.${key}`, "like", `%${value}%`);
+      }
+    }
+  }
+  return query;
 }
