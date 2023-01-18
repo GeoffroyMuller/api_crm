5003dc176a3e1a41843d75447339ed54fe7ae3f0
etienne1698 <etienne.robert1698@gmail.com>
2023-01-18 18:13:32 +0100

    update event model

--- src/api/events/event.model.ts
+++ src/api/events/event.model.ts
@@ -1,7 +1,21 @@
 import { Model } from "objection";
 import Company from "../companies/company.model";
 
+
 export default class Event extends Model {
+  id?: number;
+  dtstamp?: string;
+  dtstart?: string;
+  dtend?: string;
+  summary?: string;
+  description?: string;
+  location?: string;
+  rrule?: string;
+  organizer?: string;
+  attendee?: string;
+  status?: string;
+
+
   idCompany?: number;
   company?: Company;
 
