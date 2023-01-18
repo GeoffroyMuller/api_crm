caf008281eec7a1ec1e1fb31052daba3cdc1a3be
etienne1698 <etienne.robert1698@gmail.com>
2023-01-18 17:53:12 +0100

    up

--- src/config/migrations/20230118035427_event.ts
+++ src/config/migrations/20230118035427_event.ts
@@ -5,9 +5,9 @@ const tableName = "events";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(tableName, (table) => {
     table.increments("id");
-    table.timestamp("dtstamp");
-    table.timestamp("dtstart");
-    table.timestamp("dtend");
+    table.date("dtstamp");
+    table.date("dtstart");
+    table.date("dtend");
     table.string("summary");
     table.string("description");
     table.string("location");
