7183bc07504d8c104279fae4c4fbc7378c8d7117
geoff <geoffroym2@gmail.com>
2023-02-06 18:55:04 +0100

    up relation sale line

--- src/api/sales/sale_line.model.ts
+++ src/api/sales/sale_line.model.ts
@@ -1,45 +1,41 @@
-import { Model } from "objection"
+import { Model } from "objection";
 import Product from "../products/product.model";
 import Vat from "../vats/vat.model";
+import SaleSubline from "./sale_subline.model";
 
 export default class SaleLine extends Model {
-   id?: number;
-   idSale?: number;
-   idProduct?: number;
-   description?: string;
-   qty?: number;
-   unit_price?: number;
-   discount?: number;
-   discount_type?: '€' | '%';
-   type?: "title" | "product" | "comment" | "discount"
-   idVat?: number;
-   order?: number;
+  id?: number;
+  idSale?: number;
+  idProduct?: number;
+  description?: string;
+  qty?: number;
+  unit_price?: number;
+  discount?: number;
+  discount_type?: "€" | "%";
+  type?: "title" | "product" | "comment" | "discount";
+  idVat?: number;
+  order?: number;
 
-   product?: Product;
-   vat?: Vat;
+  product?: Product;
+  vat?: Vat;
 
+  sublines?: Array<SaleSubline>;
 
-    static get tableName() {
-        return 'sale_lines'
-    }
+  static get tableName() {
+    return "sale_lines";
+  }
 
-    static relationMappings = {
-        vat: {
-            relation: Model.BelongsToOneRelation,
-            modelClass: Vat,
-            join: {
-                from: SaleLine.tableName + '.idVat',
-                to: 'vat.id'
-            }
+  static get relationMappings() {
+    const SaleSublineModel = require("../sales/sale_subline.model").default;
+    return {
+      sublines: {
+        relation: Model.HasManyRelation,
+        modelClass: SaleSublineModel,
+        join: {
+          from: SaleLine.tableName + ".id",
+          to: "sale_sublines.idSaleLine",
         },
-        product: {
-            relation: Model.BelongsToOneRelation,
-            modelClass: Product,
-            join: {
-                from: SaleLine.tableName + '.idProduct',
-                to: 'products.id'
-            }
-        }
+      },
     };
+  }
 }
-
