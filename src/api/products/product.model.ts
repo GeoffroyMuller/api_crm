9a381ef1ae0a4c0746ca79a18a6a34cb2aaca600
etienne1698 <etienne.robert1698@gmail.com>
2023-02-07 21:17:05 +0100

    add product vat

--- src/api/products/product.model.ts
+++ src/api/products/product.model.ts
@@ -1,10 +1,12 @@
 import { Model } from "objection";
 import ProductField from "./product_field.model";
 import ProductReal from "../products_real/product_real.model";
+import Vat from "../vats/vat.model";
 
 export default class Product extends Model {
   id?: number;
   idCompany?: number;
+  idVat?: number;
   name?: string;
   description?: string;
   price?: number;
@@ -34,6 +36,14 @@ export default class Product extends Model {
         join: {
           from: "products.id",
           to: "product_fields.idProduct",
+        },
+      },
+      vat: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Vat,
+        join: {
+          from: "products.idVat",
+          to: "vat.id",
         },
       },
     };
