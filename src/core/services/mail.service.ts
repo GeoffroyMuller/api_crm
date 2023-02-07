601c8ef76591323cd91d358144d95dc018949536
etienne1698 <etienne.robert1698@gmail.com>
2023-02-08 00:48:38 +0100

    update mail

--- src/core/services/mail.service.ts
+++ src/core/services/mail.service.ts
@@ -5,19 +5,11 @@ const mailService: IMailService = {
     async sendMail(props) {
         console.log("send_mail", props);
         // create reusable transporter object using the default SMTP transport
-        const transporter = createTransport({
-            host: "localhost",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: "bot@apicrm.demoapp.fr",
-                pass: "0kot%H049",
-            },
-        });
+        const transporter = createTransport();
         
         return transporter.sendMail(
             {
-                from: " ",
+                from: "bot@apicrm.demoapp.fr",
                 to: props.to,
                 subject: props.subject,
                 html: props.html,
