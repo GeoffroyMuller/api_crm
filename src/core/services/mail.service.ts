abda6c62b56421a1c1d15fee342a75b8d56fbf64
etienne1698 <etienne.robert1698@gmail.com>
2023-02-08 01:17:49 +0100

    update

--- src/core/services/mail.service.ts
+++ src/core/services/mail.service.ts
@@ -5,13 +5,16 @@ const mailService: IMailService = {
     async sendMail(props) {
 
         const transporter = createTransport({
-            host: "smtp.demoapp.fr",
-            port: 587,
-            secure: false, // true for 465, false for other ports
+            host: "apicrm.demoapp.fr",
+            port: 465,
+            secure: true, // true for 465, false for other ports
             auth: {
                 user: "bot@apicrm.demoapp.fr",
                 pass: "0kot%H049",
             },
+            tls: {
+			rejectUnauthorized: false,//Laisser à false si vos certificat ne sont pas à jour
+        },
         });
         
         return transporter.sendMail(
