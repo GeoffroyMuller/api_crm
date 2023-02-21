c73b878e7dd2acd736bee8bc3a4a8c3b7a93f8d6
etienne1698 <etienne.robert1698@gmail.com>
2023-02-21 20:33:20 +0100

    paginate users

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.use(authMiddleware);
 
-router.get('/', controller.getAll)
+router.get('/', controller.paginate)
 router.post('/', controller.create)
 router.put('/:id', controller.update)
 router.delete('/:id', controller.delete)
